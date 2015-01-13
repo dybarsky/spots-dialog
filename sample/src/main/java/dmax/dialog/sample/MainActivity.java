50af419fdfea0c0ec39983b372ef8c9d11a553ba
Maxim Dybarsky <maxim.dybarskyy@gmail.com>
2015-01-13 17:04:55 +0100

    Add buttons to sample app for show default/styled dialog

--- sample/src/main/java/dmax/dialog/sample/MainActivity.java
+++ sample/src/main/java/dmax/dialog/sample/MainActivity.java
@@ -2,6 +2,7 @@ package dmax.dialog.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import dmax.dialog.SpotsDialog;
 
@@ -9,13 +10,27 @@ import dmax.dialog.SpotsDialog;
  * Created by Maxim Dybarsky | maxim.dybarskyy@gmail.com
  * on 13.01.15 at 14:32
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        new SpotsDialog(this).show();
-        new SpotsDialog(this, R.style.Custom).show();
+        setContentView(R.layout.main);
+
+        findViewById(android.R.id.button1).setOnClickListener(this);
+        findViewById(android.R.id.button2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case android.R.id.button1:
+                new SpotsDialog(this).show();
+                break;
+            case android.R.id.button2:
+                new SpotsDialog(this, R.style.Custom).show();
+                break;
+        }
     }
 }
